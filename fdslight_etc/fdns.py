e0d02e8978f5418d92b81e454f8e44d08ad18b08
fdslight <freekai@outlook.com>
2016-02-09 20:31:04 +0800

    2016/2/9

--- fdslight_etc/fdns.py
+++ fdslight_etc/fdns.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+# DNS代理配置
 configs = {
     # 透明传输的dns地址
-    "transparent_dns": "192.168.2.1",
+    "transparent_dns": "192.168.1.1",
     # 加密dns地址
     "encrypt_dns": "8.8.8.8",
     # 本地监听地址
-    "bind_addr": "192.168.2.8"
+    "bind_addr": "0.0.0.0"
 }
