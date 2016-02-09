e9e47d790e3083c6f9a9d8330f7aa66f9e714e1a
fdslight <freekai@outlook.com>
2016-02-09 20:32:31 +0800

    2016/2/9

--- fdslight_etc/fn_client.py
+++ fdslight_etc/fn_client.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 configs = {
     "tcp_server_address": (
-        "vof.freekai.net",8878
+        # 你的服务器的地址以及端口
+        "youserver.domain", 8964
     ),
     "tunnelc": "tunnelc_simple",
     "tunnelc_simple": {
