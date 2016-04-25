8c33c88f1df5712a94f2f572c9d4e992e261af3b
freekai <freekai@outlook.com>
2016-04-25 20:48:56 +0800

    ...

--- test.py
+++ test.py
@@ -1,110 +1,110 @@
 #!/usr/bin/env python3
-import socket
-import pywind.lib.timer as timer
-import freenet.lib.file_parser as file_parser
+"""
+import dns.resolver
+
+r = dns.resolver.Resolver()
+r.nameservers = ["192.168.1.100", ]
+
+for qss in r.query("www.google.com"):
+    print(qss)
+"""
+"""
+fdst = open("./fdslight_etc/blacklist.txt", "rb")
+results = []
+
+for line in fdst:
+    if line[0] == ord('#'):
+        results.append(line)
+        continue
+    line=line.replace(b"\r",b'')
+    line=line.replace(b"\n",b'')
+    if not line:
+        results.append(b"\r\n")
+        continue
+    nline = b"".join((line,b":1\r\n",))
+    results.append(nline)
+
+fdst.close()
+fdst=open("./blacklist.txt","wb")
+for line in results:fdst.write(line)
+fdst.close()
+"""
 
 
-def ip4b_2_number(ip_pkt):
-    """ipv4 bytes转换为数字"""
-    return (ip_pkt[0] << 24) | (ip_pkt[1] << 16) | (ip_pkt[2] << 8) | ip_pkt[3]
-
-
-class _udp_whitelist(object):
-    """UDP白名单类"""
-    __tree = None
-    # 缓存回收超时
-    __CACHE_TIMEOUT = 180
-    __timer = None
-
-    __cache = None
+class _host_match(object):
+    """对域名进行匹配,以找到是否在符合的规则列表中
+    """
+    __rules = None
 
     def __init__(self):
-        self.__tree = {}
-        self.__timer = timer.timer()
-        self.__cache = {}
+        self.__rules = {}
 
-    def add_rule(self, ipaddr, mask):
-        if mask < 1 or mask > 32: raise ValueError("the value of mask is wrong")
-        ippkt = socket.inet_aton(ipaddr)
+    def add_rule(self, host_rule):
+        host, flags = host_rule
+        tmplist = host.split(".")
+        tmplist.reverse()
 
-        tmp_dict = self.__tree
+        if not tmplist:
+            return
 
-        a = int(mask / 8)
-        r = mask % 8
-        if r: a += 1
+        lsize = len(tmplist)
+        n = 0
+        tmpdict = self.__rules
 
-        for i in range(4):
-            n = ippkt[i]
-
-            if i + 1 == a:
-                if "values" not in tmp_dict: tmp_dict["values"] = {}
-                if mask not in tmp_dict["values"]: tmp_dict["values"][mask] = []
-                tmp_dict["values"][mask].append(n)
-                break
-
-            if n not in tmp_dict:
-                tmp_dict[n] = {}
-
-            tmp_dict = tmp_dict[n]
-
-        return
-
-    def __add_to_cache(self, ippkt, from_wl=True):
-        self.__cache[ippkt] = from_wl
-        self.__timer.set_timeout(ippkt, self.__CACHE_TIMEOUT)
-
-    def __get_subn(self, a_list, b):
-        cnt = 24
-        ret_v = 0
-
-        for n in a_list:
-            ret_v |= n << cnt
-            cnt -= 8
-
-        return ret_v | (b << cnt)
-
-    def find(self, ippkt):
-        if ippkt in self.__cache: return self.__cache[ippkt]
-
-        tmp_dict = self.__tree
-        t_net_v = ip4b_2_number(ippkt)
-
-        values = []
-        _values = None
-
-        for n in ippkt:
-            if n not in tmp_dict:
-                if "values" not in tmp_dict:
-                    self.__add_to_cache(ippkt, from_wl=False)
-                    return False
-                _values = tmp_dict["values"]
-                break
-            values.append(n)
-            tmp_dict = tmp_dict[n]
-
-        is_find = False
-
-        for m in _values:
-            mask_v = 0
-            for i in range(m): mask_v |= 1 << (31 - i)
-            for t in _values[m]:
-                subn = self.__get_subn(values, t)
-                if t_net_v & mask_v == subn:
-                    is_find = True
+        old_name = ""
+        old_dict = tmpdict
+        while n < lsize:
+            name = tmplist[n]
+            if name not in tmpdict:
+                if name == "*" or n == lsize - 1:
+                    old_dict[old_name] = {name: flags}
                     break
-                ''''''
-            ''''''
-        self.__add_to_cache(ippkt, from_wl=is_find)
+                old_dict = tmpdict
+                tmpdict[name] = {}
+            if name == "*":
+                n += 1
+                continue
+            old_name = name
+            tmpdict = tmpdict[name]
+            n += 1
 
-        return is_find
-
-    def recycle_cache(self):
-        names = self.__timer.get_timeout_names()
-        for name in names:
-            if name in self.__cache: del self.__cache[name]
-            if self.__timer.exists(name): self.__timer.drop(name)
         return
 
-    def print_tree(self):
-        print(self.__tree)
+    def match(self, host):
+        tmplist = host.split(".")
+        tmplist.reverse()
+        # 加一个空数据，用以匹配 xxx.xx这样的域名
+        tmplist.append("")
 
+        is_match = False
+        flags = 0
+
+        tmpdict = self.__rules
+        for name in tmplist:
+            if "*" in tmpdict:
+                is_match = True
+                flags = tmpdict["*"]
+                break
+            if name not in tmpdict: break
+            v = tmpdict[name]
+            if type(v) != dict:
+                is_match = True
+                flags = v
+                break
+            tmpdict = v
+
+        return (is_match, flags,)
+
+    def print_dict(self):
+        print(self.__rules)
+
+
+import freenet.lib.file_parser as file_parser
+
+h = _host_match()
+rules = file_parser.parse_host_file("fdslight_etc/blacklist.txt")
+
+for rule in rules: h.add_rule(rule)
+
+h.print_dict()
+print(h.match("googledrive.com"))
