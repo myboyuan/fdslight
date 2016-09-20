bc3693619b5c4bedb66cb7b0d0ff217741b50531
fdslight <freekai@outlook.com>
2016-09-20 23:41:17 +0800

    ...

--- pywind/jsonrpc/handler/rpc_base.py
+++ pywind/jsonrpc/handler/rpc_base.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 
+class RPCNotFoundMethodErr(Exception):
+    pass
+
+class RPCInvalidParamsErr(Exception):pass
 
 class base_handler(object):
-    def register_function(self, name, func):
+    def register_function(self, name, func, module=None):
         pass
 
-    def register_functions(self, seq):
+    def func_exists(self, func_name):
         pass
+
+    def call_func(self, func_name, params):
+        pass
+
