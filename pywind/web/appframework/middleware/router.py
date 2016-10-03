9aacf8ca35d5d252e4f00bc4de137173ada44905
fdslight <freekai@outlook.com>
2016-10-03 20:21:26 +0800

    ...

--- pywind/web/appframework/middleware/router.py
+++ pywind/web/appframework/middleware/router.py
@@ -3,32 +3,9 @@
 
 class base(object):
     """路由基本类,重写这个方法以适合你自己的路由需求"""
-    __app = None
-    __configs = None
-    __bootstrap = None
+    def __call__(self, environ, start_response):
+        self.app_call(environ, start_response)
 
-    def __init__(self, app, bootstrap, configs=None):
-        """
-        :param app:
-        :param bootstrap: 引导handler
-        :param configs:
-        """
-        self.__app = app
-        self.__configs = configs
-        self.__bootstrap = bootstrap
-
-    @property
-    def app(self): return self.__app
-
-    @property
-    def bootstrap(self): return self.__bootstrap
-
-    @property
-    def configs(self): return self.__configs
-
-    def __call__(self, start_response, environ):
-        self.app_call(start_response, environ)
-
-    def app_call(self, start_response, environ):
+    def app_call(self, environ, start_response):
         """重写这个方法"""
         pass
--- pywind/web/appserver.py
+++ pywind/web/appserver.py
@@ -4,14 +4,16 @@ import pywind.web.handler.scgi as scgi
 
 
 class appserver(dispatcher.dispatcher):
+    __configs = None
+
+    def __init__(self, configs):
+        super(appserver, self).__init__()
+        self.__configs = configs
+
     def debug_run(self):
         self.create_poll()
-        fd = self.create_handler(-1, scgi.scgid_listen, {})
+        fd = self.create_handler(-1, scgi.scgid_listen, self.__configs)
         self.get_handler(fd).after()
 
     def init_func(self):
         self.debug_run()
-
-
-app = appserver()
-app.ioloop()
