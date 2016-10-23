5d5f12ca641c4195c699aae484856fff0b3e9772
fdslight <freekai@outlook.com>
2016-10-23 23:12:14 +0800

    ...

--- pywind/proc/lib/jsonrpc.py
+++ pywind/proc/lib/jsonrpc.py
@@ -143,3 +143,65 @@ class jsonrpc_parser(object):
         self.__is_call = False
         self.__is_return_error = False
         self.__is_return_ok = False
+
+    def get_function_call(self, pydict):
+        return (pydict["method"], (), {})
+
+    def get_function_return(self, pydict):
+        return pydict["result"]
+
+
+class RPCNotFoundMethodErr(Exception):
+    pass
+
+
+class RPCInvalidParamsErr(Exception): pass
+
+
+class func_call(object):
+    __functions = None
+
+    def __init__(self):
+        self.__functions = {}
+        self.__functions["::"] = {}
+
+    def register_function(self, name, func, module=None):
+        if not module:
+            self.__functions["::"][name] = func
+        else:
+            self.__functions[module][name] = func
+        return
+
+    def call_func(self, name, params):
+        module, func_name = self.__get_func(name)
+        if not module: module = "::"
+
+        try:
+            mod_functions = self.__functions[module]
+        except KeyError:
+            raise RPCNotFoundMethodErr("cannot found module %s" % module)
+
+        try:
+            func = mod_functions[func_name]
+        except KeyError:
+            raise RPCNotFoundMethodErr("cannot found method %s" % func_name)
+
+        result = None
+
+        try:
+            if isinstance(params, dict):
+                result = func(**params)
+            else:
+                result = func(*params)
+        except TypeError:
+            raise RPCInvalidParamsErr("function %s params error" % name)
+        return result
+
+    def __get_func(self, name):
+        pos = name.find("::")
+        if pos < 0: return (None, name,)
+
+        mod_name = name[0:pos]
+        pos += 2
+
+        return (mod_name, name[pos:])
