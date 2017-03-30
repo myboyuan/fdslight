54b1f7e94f928a6e499866d16fc1c94e4cfb0d97
fdslight <freekai@outlook.com>
2017-03-30 16:59:05 +0800

    ...

--- pywind/RPC/lib/rpc_client.py
+++ pywind/RPC/lib/rpc_client.py
@@ -10,10 +10,29 @@ class _namespace(object):
 
 class _resource(object):
     __obj_id = None
-    name = ""
+    __func_name = None
+    __sent_func = None
 
     def __init__(self, sent_func, object_id):
         self.__obj_id = object_id
+        self.__sent_func = sent_func
 
     def __getattr__(self, item):
-        print(item)
+        self.__func_name = item
+
+        return self
+
+    def __call__(self, *args, **kwargs):
+        return self.__sent_func(self.__func_name, *args, **kwargs)
+
+
+class rpc_client(object):
+    def get_ns(self, ns):
+        """获取命名空间
+        :param ns: 
+        :return: 
+        """
+        pass
+
+    def __send_request(self, func_name, *args, **kwargs):
+        pass
