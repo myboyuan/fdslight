97da88fdb8522afe50ccf741b801756cbfe68063
fdslight <freekai@outlook.com>
2017-03-30 16:33:03 +0800

    ...

--- pywind/RPC/handlers/rpcd.py
+++ pywind/RPC/handlers/rpcd.py
@@ -1,1 +1,179 @@
 #!/usr/bin/env python3
+
+import pywind.evtframework.handlers.tcp_handler as tcp_handler
+import pywind.RPC.lib.protocol as rpc_protocol
+
+import time
+
+
+class rpcd_listener(tcp_handler.tcp_handler):
+    def init_func(self, creator, listen_address):
+        pass
+
+    def tcp_accept(self):
+        pass
+
+
+class rpc_handler(tcp_handler.tcp_handler):
+    # 用户保存的对象,用于对类实例,文件这些调用状态的保存
+    __objects = None
+
+    __LOOP_TIMEOUT = 60
+
+    __update_time = None
+    __conn_timeout = None
+
+    __builder = None
+    __parser = None
+
+    def init_func(self, creator, cs, address, conn_timeout=600):
+        self.__objects = {}
+        self.__update_time = time.time()
+        self.__conn_timeout = conn_timeout
+
+        self.__parser = rpc_protocol.parser()
+
+        self.set_socket(cs)
+        self.register(self.fileno)
+        self.add_evt_read(self.fileno)
+
+        return
+
+    def set_object(self, obj_id, o, del_func):
+        """设置对象
+        :param obj_id: 对象ID
+        :param o: 对象实例
+        :param del_func:资源释放函数
+        :return: 
+        """
+        self.__objects[obj_id] = (o, del_func,)
+
+    def __del_objects(self):
+        for k, v in self.__objects.items():
+            _, del_func = v
+            # 调用资源释放函数进行资源释放
+            del_func()
+        return
+
+    def tcp_delete(self):
+        self.__del_objects()
+        self.unregister(self.fileno)
+        self.close()
+
+    def tcp_timeout(self):
+        t = time.time()
+        if t - self.__update_time > self.__conn_timeout:
+            self.delete_handler(self.fileno)
+            return
+        self.set_timeout(self.fileno, self.__LOOP_TIMEOUT)
+
+    def tcp_writable(self):
+        self.remove_evt_write(self.fileno)
+
+    def tcp_readable(self):
+        rdata = self.reader.read()
+
+        self.__parser.put_data(rdata)
+
+        while 1:
+            self.__parser.parse()
+            rs = self.__parser.get_result()
+            if not rs: break
+            token_id, content = rs
+            if not self.is_permit_access(token_id):
+                self.delete_handler(self.fileno)
+                return
+            if not self.__builder:
+                self.__builder = rpc_protocol.builder(token_id)
+
+            b = self.__handle_function_call(content)
+            if not b:
+                self.delete_handler(self.fileno)
+                return
+        self.__update_time = time.time()
+
+    def tcp_error(self):
+        self.delete_handler(self.fileno)
+
+    def __handle_function_call(self, content):
+        try:
+            sts = content.decode()
+        except UnicodeDecodeError:
+            return False
+        try:
+            pydict = rpc_protocol.parse_function_call(sts)
+        except rpc_protocol.ProtocolErr:
+            return False
+
+        call_id = pydict["call_id"]
+        namespace = pydict["namespace"]
+        func_name = pydict["function"]
+        args = pydict["args"]
+        kwargs = pydict["kwargs"]
+
+        self.handle_function_call(
+            call_id, namespace, func_name, *tuple(args), **kwargs
+        )
+
+        return True
+
+    def get_object(self, obj_id):
+        """获取对象
+        :param obj_id: 
+        :return: 
+        """
+        return self.__objects.get(obj_id, None)
+
+    def del_object(self, obj_id):
+        """删除对象
+        注意:调用此函数不会自动调用资源释放函数
+        :param obj_id: 
+        :return: 
+        """
+        if obj_id not in self.__objects: return
+
+        del self.__objects[obj_id]
+
+    def object_exists(self, obj_id):
+        """检查对象是否存在
+        :param obj_id: 
+        :return: 
+        """
+        return obj_id in self.__objects
+
+    def is_permit_access(self, token_id):
+        """重写这个方法,是否允许用户访问
+        :param token_id: 
+        :return Boolean: True表示允许访问,False表示禁止访问 
+        """
+        return True
+
+    def handle_function_call(self, call_id, namespace, func_name, *args, **kwargs):
+        """处理函数调用,重写这个方法
+        :param call_id:
+        :param namespace: 
+        :param func_name: 
+        :param args: 
+        :param kwargs: 
+        :return: 
+        """
+        pass
+
+    def return_function_result(self, call_id, return_val=None, is_class=False, is_err=None, err_code=None):
+        """返回函数结果
+        :param call_id: 
+        :param return_val: 
+        :param is_class:
+        :param is_err: 
+        :param err_code: 
+        :return: 
+        """
+        sts = rpc_protocol.build_function_return(
+            call_id, return_val=return_val, is_class=is_class,
+            is_err=is_err, err_code=err_code
+        )
+
+        byte_data = sts.encode()
+
+        self.add_evt_write(self.fileno)
+        self.writer.write(byte_data)
--- pywind/RPC/lib/protocol.py
+++ pywind/RPC/lib/protocol.py
@@ -3,11 +3,11 @@
 RPC通讯协议
 数据头部如下:
 token_id: 16 bytes token id用于验证用户是否允许访问
-packet_type:1 bytes 包类型
 data_len:2 bytes  数据长度
 """
 
 import json, hashlib
+import pywind.lib.reader as reader
 
 #### 故障码大全
 
@@ -35,8 +35,18 @@ class MethodNotFoundErr(RPCErr):
 
 
 def gen_token_id(sts_list):
-    pass
+    """生成token id
+    :param sts_list: 字符串列表
+    :return: 
+    """
+    byte_sts = []
+    for sts in sts_list:
+        byte_sts.append(sts.encode("iso-8859-1"))
 
+    m = hashlib.md5()
+    m.update(b"".join(byte_sts))
+
+    return m.digest()
 
 
 def build_function_call(call_id, namespace, func_name, *args, **kwargs):
@@ -59,10 +69,11 @@ def build_function_call(call_id, namespace, func_name, *args, **kwargs):
     return json.dumps(pydict)
 
 
-def build_function_return(call_id, return_val=None, is_err=False, err_code=None):
+def build_function_return(call_id, return_val=None, is_class=False, is_err=False, err_code=None):
     """构建函数返回值
     :param call_id: 
     :param return_val: 
+    :param is_class:是否是类实例
     :param is_err:是否发生故障
     :param err_code:故障码
     :return: 
@@ -72,6 +83,7 @@ def build_function_return(call_id, return_val=None, is_err=False, err_code=None)
         "return": return_val,
         "is_err": is_err,
         "err_code": err_code,
+        "is_class": is_class
     }
 
     return json.dumps(pydict)
@@ -109,3 +121,82 @@ def parse_function_call(sts):
         if name not in pyobj: raise ProtocolErr
 
     return pyobj
+
+
+HDR_LENGTH = 18
+
+
+class parser(object):
+    __reader = None
+    __is_parsed_header = False
+
+    __content_length = 0
+    __token_id = None
+    __results = None
+
+    def __init__(self):
+        self.__is_parsed_header = False
+        self.__reader = reader.reader()
+        self.__results = []
+
+    def put_data(self, byte_data):
+        self.__reader._putvalue(byte_data)
+
+    def parse(self):
+        if not self.__is_parsed_header:
+            if self.__reader.size() < HDR_LENGTH:
+                return
+            self.__token_id = self.__reader.read(16)
+            t = self.__reader.read(2)
+            self.__content_length = (t[0] << 8) | t[1]
+            self.__is_parsed_header = True
+
+        if self.__reader.size() < self.__content_length: return
+
+        self.__results.append(
+            (self.__token_id, self.__reader.read(self.__content_length).decode())
+        )
+        self.__is_parsed_header = False
+
+    def get_result(self):
+        rs = None
+        try:
+            rs = self.__results.pop(0)
+        except IndexError:
+            pass
+        return rs
+
+
+class builder(object):
+    __token_id = None
+
+    def __init__(self, token_id):
+        if len(token_id) != 16: raise ValueError("wrong token id length")
+        self.__token_id = token_id
+
+    def build_data(self, sts):
+        byte_data = sts.encode()
+        content_length = len(byte_data)
+
+        return b"".join(
+            [
+                self.__token_id,
+                bytes([(content_length & 0xff00) >> 8, content_length & 0xff, ]),
+                byte_data
+            ]
+        )
+
+
+"""
+b = builder(gen_token_id("hello"))
+rs = b.build_data("nihao")
+
+p = parser()
+p.put_data(rs)
+
+while 1:
+    p.parse()
+    t = p.get_result()
+    if not t: break
+    print(t)
+"""
