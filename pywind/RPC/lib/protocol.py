2aab329a55d7ab5a83822e5829b9267f411981e5
fdslight <freekai@outlook.com>
2017-03-30 15:03:47 +0800

    ...

--- pywind/RPC/lib/protocol.py
+++ pywind/RPC/lib/protocol.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 """
 RPC通讯协议
+数据头部如下:
+token_id: 16 bytes token id用于验证用户是否允许访问
+packet_type:1 bytes 包类型
+data_len:2 bytes  数据长度
 """
 
-import json
+import json, hashlib
 
 #### 故障码大全
 
@@ -12,6 +16,27 @@ ERR_NO_NAMESPACE = 1
 
 # 方法找不到
 ERR_NO_METHOD = 2
+
+
+class RPCErr(Exception):
+    pass
+
+
+class ProtocolErr(RPCErr):
+    pass
+
+
+class NsNotFoundErr(RPCErr):
+    pass
+
+
+class MethodNotFoundErr(RPCErr):
+    pass
+
+
+def gen_token_id(sts_list):
+    pass
+
 
 
 def build_function_call(call_id, namespace, func_name, *args, **kwargs):
@@ -52,9 +77,35 @@ def build_function_return(call_id, return_val=None, is_err=False, err_code=None)
     return json.dumps(pydict)
 
 
-def parse_function_call(sts):
-    pass
-
-
 def parse_function_return(sts):
-    pass
+    try:
+        pyobj = json.loads(sts)
+    except json.JSONDecodeError:
+        raise ProtocolErr
+
+    fields = ["call_id", "return", "is_err", "err_code"]
+    for name in fields:
+        if name not in pyobj: raise ProtocolErr
+
+    err_code = pyobj["err_code"]
+
+    if pyobj["is_err"]:
+        if err_code == ERR_NO_NAMESPACE:
+            raise NsNotFoundErr
+        if err_code == ERR_NO_METHOD:
+            raise MethodNotFoundErr
+
+    return pyobj
+
+
+def parse_function_call(sts):
+    try:
+        pyobj = json.loads(sts)
+    except json.JSONDecodeError:
+        raise ProtocolErr
+
+    fields = ["namespace", "call_id", "function", "args", "kwargs"]
+    for name in fields:
+        if name not in pyobj: raise ProtocolErr
+
+    return pyobj
