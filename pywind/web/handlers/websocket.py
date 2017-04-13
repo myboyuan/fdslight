f7adc98a02dfc13bb7b464736ae2953299ee6bf1
fdslight <freekai@outlook.com>
2017-04-14 00:45:20 +0800

    ...

---
+++ pywind/web/handlers/websocket.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import pywind.evtframework.handlers.tcp_handler as tcp_handler
+
+
+class ws_listener(tcp_handler.tcp_handler):
+    pass
+
+
+class _ws_handler(tcp_handler.tcp_handler):
+    pass
