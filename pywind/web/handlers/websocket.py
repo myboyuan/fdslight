b268a3cd569cf60b5157eb74b522625a5d63f014
fdslight <freekai@outlook.com>
2017-04-23 20:01:10 +0800

    ...

--- pywind/web/handlers/websocket.py
+++ pywind/web/handlers/websocket.py
@@ -2,6 +2,7 @@
 
 import pywind.evtframework.handlers.tcp_handler as tcp_handler
 import pywind.web.lib.websocket as websocket
+import pywind.web.lib.httputils as httputils
 import socket
 
 
@@ -12,7 +13,7 @@ class ws_listener(tcp_handler.tcp_handler):
         else:
             fa = socket.AF_INET
 
-        s = socket.socket(fa, socket.SOCK_DGRAM)
+        s = socket.socket(fa, socket.SOCK_STREAM)
         if is_ipv6: s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 1)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
@@ -50,13 +51,23 @@ class ws_handler(tcp_handler.tcp_handler):
     __encoder = None
     __decoder = None
 
+    __is_handshake = None
+
+    # 自定义的握手响应头
+    __ext_handshake_resp_headers = None
+
     def init_func(self, creator, cs, caddr):
         self.__caddr = caddr
 
-        self.__decoder = websocket.decoder()
-        self.__encoder = websocket.encoder()
+        self.__decoder = websocket.decoder(server_side=True)
+        self.__encoder = websocket.encoder(server_side=True)
+
+        self.__is_handshake = False
+        self.__ext_handshake_resp_headers = []
 
         self.set_socket(cs)
+        self.register(self.fileno)
+        self.add_evt_read(self.fileno)
 
         return self.fileno
 
@@ -64,20 +75,125 @@ class ws_handler(tcp_handler.tcp_handler):
     def caddr(self):
         return self.__caddr
 
+    def response_error(self):
+        resp_sts = httputils.build_http1x_resp_header(
+            "400 Bad Request",
+            [
+                ("Sec-WebSocket-Version", 13),
+            ],
+            version="1.1"
+        )
+
+        self.writer.write(resp_sts.encode("iso-8859-1"))
+        self.add_evt_write(self.fileno)
+
+        self.delete_this_no_sent_data()
+
+    def __do_handshake(self, byte_data):
+        try:
+            sts = byte_data.decode("iso-8859-1")
+        except UnicodeDecodeError:
+            self.response_error()
+            return False
+
+        try:
+            rs = httputils.parse_htt1x_request_header(sts)
+        except:
+            self.response_error()
+            return False
+
+        req, headers = rs
+
+        dic = {}
+        for k, v in headers:
+            k = k.lower()
+            dic[k] = v
+
+        if "sec-websocket-key" not in dic: return False
+        ws_version = dic.get("sec-websocket-version", 0)
+
+        is_err = False
+        try:
+            ws_version = int(ws_version)
+            if ws_version != 13: is_err = True
+        except ValueError:
+            is_err = True
+        if is_err:
+            self.response_error()
+            return False
+
+        if not self.on_handshake(req, headers):
+            self.response_error()
+            return False
+
+        sec_ws_key = dic["sec-websocket-key"]
+        resp_sec_key = websocket.gen_handshake_key(sec_ws_key)
+
+        resp_headers = [
+            ("Upgrade", "websocket"),
+            ("Connection", "Upgrade"),
+            ("Sec-WebSocket-Accept", resp_sec_key)
+        ]
+
+        resp_headers += self.__ext_handshake_resp_headers
+
+        resp_sts = httputils.build_http1x_resp_header(
+            "101 Switching Protocols",
+            resp_headers,
+            version="1.1"
+        )
+
+        self.writer.write(resp_sts.encode("iso-8859-1"))
+        self.add_evt_write(self.fileno)
+
+        return True
+
+    def on_handshake(self, request, headers):
+        """重写这个方法
+        :param request: 
+        :param headers: 
+        :return Boolean: False表示握手不允许,True表示握手允许 
+        """
+        return True
+
+    def set_handshake_resp_header(self, name, value):
+        """设置额外的响应头
+        :param name: 
+        :param value: 
+        :return: 
+        """
+        self.__ext_handshake_resp_headers.append((name, value,))
+
     def set_ws_timeout(self, timeout):
         self.__conn_timeout = timeout
 
     def tcp_readable(self):
         rdata = self.reader.read()
 
+        if not self.__is_handshake:
+            if not self.__do_handshake(rdata): return
+            self.__is_handshake = True
+            return
+
+        self.__decoder.input(rdata)
+        self.ws_readable()
+
+    @property
+    def decoder(self):
+        return self.__decoder
+
+    @property
+    def encoder(self):
+        return self.__encoder
 
     def tcp_writable(self):
-        pass
+        self.remove_evt_write(self.fileno)
 
     def tcp_error(self):
         self.delete_handler(self.fileno)
 
     def tcp_delete(self):
+        self.ws_close()
         self.unregister(self.fileno)
         self.close()
 
@@ -100,7 +216,13 @@ class ws_handler(tcp_handler.tcp_handler):
         """重写这个方法
         :return: 
         """
-        pass
+        while self.decoder.continue_parse():
+            self.decoder.parse()
+
+            if self.decoder.frame_ok():
+                data = self.decoder.get_data()
+                self.decoder.reset()
+                print(data)
 
     def ws_writable(self):
         """重写这个方法
--- pywind/web/lib/httputils.py
+++ pywind/web/lib/httputils.py
@@ -3,9 +3,11 @@
 class Http1xHeaderErr(Exception): pass
 
 
-def build_http1x_resp_header(status, seq):
-    tmplist = ["%s HTTP/1.1\r\n" % status]
-    for k, v in seq: sts = "%s: %s\r\n" % (k, v,)
+def build_http1x_resp_header(status, seq, version="1.1"):
+    tmplist = ["HTTP/%s %s\r\n" % (version,status)]
+    for k, v in seq:
+        sts = "%s: %s\r\n" % (k, v,)
+        tmplist.append(sts)
     tmplist.append("\r\n")
 
     return "".join(tmplist)
@@ -24,7 +26,7 @@ def get_http1x_map(sts):
     tmplist = sts.split("\r\n")
 
     results = []
-    __drop_nul_seq_elements(tmplist)
+    tmplist = __drop_nul_seq_elements(tmplist)
 
     for s in tmplist:
         p = s.find(":")
@@ -38,11 +40,13 @@ def get_http1x_map(sts):
 
 
 def __drop_nul_seq_elements(seq):
-    size = len(seq)
-    for n in range(size):
-        s = seq[n]
-        if not s: seq.pop(n)
-    return
+    new_seq = []
+
+    for s in seq:
+        s = s.lstrip()
+        if s: new_seq.append(s)
+
+    return new_seq
 
 
 def parse_htt1x_request_header(sts):
@@ -51,13 +55,14 @@ def parse_htt1x_request_header(sts):
     first_line = sts[0:p]
     if first_line[0] == " ": raise Http1xHeaderErr("wrong http master:%s" % first_line)
     tmplist = first_line.split(" ")
-    __drop_nul_seq_elements(tmplist)
+    tmplist = __drop_nul_seq_elements(tmplist)
+
     if len(tmplist) != 3: raise Http1xHeaderErr("wrong http master:%s" % first_line)
 
     t = tuple(tmplist)
     method, uri, version = t
     try:
-        n_ver = float(version[4:])
+        n_ver = float(version[5:])
     except ValueError:
         raise Http1xHeaderErr("wrong http version number")
 
@@ -76,7 +81,7 @@ def parse_http1x_response_header(sts):
         raise Http1xHeaderErr("wrong http response status:%s" % stcode)
 
     tmplist = first_line.split(" ")
-    __drop_nul_seq_elements(tmplist)
+    tmplist = __drop_nul_seq_elements(tmplist)
     t = tuple(tmplist)
 
     status, version = t
