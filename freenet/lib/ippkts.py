1263908a3964fd25eb9ae3d619779275d81c53e8
fdslight <freekai@outlook.com>
2017-05-06 01:18:56 +0800

    ...

--- freenet/lib/ippkts.py
+++ freenet/lib/ippkts.py
@@ -313,12 +313,13 @@ def build_udp_packets(saddr, daddr, sport, dport, message, mtu=1500, is_udplite=
             msg_len & 0x00ff,
             0, 0,
         ]
-
+    """
     if not is_udplite:
         csum = __calc_udp_csum(saddr, daddr, b"".join([bytes(udp_hdr), message]))
         udp_hdr[6] = (csum & 0xff00) >> 8
         udp_hdr[7] = csum & 0xff
         print(csum)
+    """
 
     pkt_data = b"".join(
         (bytes(udp_hdr), message,)
