33e56f08601e0b211fbdbc2d7468d0157c02ce8a
fdslight <freekai@outlook.com>
2017-05-07 00:20:36 +0800

    ...

---
+++ pywind/sktchain/node.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+### 网络链边
+SIDE_LEFT = 1
+SIDE_RIGHT = 2
+
+
+class node(object):
+    def __init__(self):
+        pass
+
+    def left(self, *args, **kwargs):
+        pass
+
+    def right(self, *args, **kwargs):
+        pass
