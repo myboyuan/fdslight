2cbae2fb5c57faddb26752ac283bf0e8c51ac0c5
fdslight <freekai@outlook.com>
2017-06-10 18:56:55 +0800

    ...

--- test.py
+++ test.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-import ssl, socket
+import socket
+import ssl
 
-host = "www.freekai.net"
-port = 443
+HOST = 'www.google.com'
+PORT = 443
 
-ctx = ssl.create_default_context()
+ctx = ssl._create_unverified_context()
 ctx.set_alpn_protocols(['h2', 'http/1.1'])
-ctx.load_verify_locations(capth="pywind/certs")
-s = socket.socket()
 
-s = ctx.wrap_socket(s, server_hostname=host)
-s.connect((host,port))
-print(s.selected_alpn_protocol())
+conn = ctx.wrap_socket(
+    socket.socket(socket.AF_INET, socket.SOCK_STREAM), server_hostname=HOST)
+conn.connect((HOST, PORT))
 
-s.close()
+print('Next protocol:', conn.selected_alpn_protocol())
