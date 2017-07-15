65183a115479062fa1a51d55a81d82c4a254c894
fdslight <freekai@outlook.com>
2017-07-15 23:19:20 +0800

    http socks5 proxy

--- test.py
+++ test.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
+import pywind.web.lib.httputils as httputils
+
+sts = b'GET / HTTP/1.1\r\nAccept: text/html, application/xhtml+xml, image/jxr, */*\r\nAccept-Language: zh-CN\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063\r\nAccept-Encoding: gzip, deflate\r\nIf-Modified-Since: Sat, 15 Jul 2017 14:59:18 GMT\r\nHost: www.youku.com\r\n\r\n'
+
 import socket
-import ssl
 
-HOST = 'www.google.com'
-PORT = 443
+s = socket.socket()
 
-ctx = ssl._create_unverified_context()
-ctx.set_alpn_protocols(['h2', 'http/1.1'])
+s.connect(("www.youku.com", 80))
 
-conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-conn.connect((HOST, PORT))
-conn = ctx.wrap_socket(conn
-                       , server_hostname=HOST)
+s.send(sts)
 
-print('Next protocol:', conn.selected_alpn_protocol())
+while 1:
+    print(s.recv(4096))
