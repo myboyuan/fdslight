2c8ada0657b1d30c0a3c981f23d11186321a989b
fdslight <freekai@outlook.com>
2019-06-07 18:56:21 +0800

    ...

--- any2ws_server.py
+++ any2ws_server.py
@@ -9,22 +9,40 @@ if not BASE_DIR: BASE_DIR = "."
 sys.path.append(BASE_DIR)
 
 import any2ws.handlers.any2tcp as any2tcp
-import any2ws.handlers.client as client
 import any2ws.any2wsd as any2wsd
+import any2ws.handlers.wsserver as wsserver
 
 PID_PATH = "%s/any2ws_server.pid" % BASE_DIR
 
 
 class any2wsd_server(any2wsd.any2wsd):
+    __wss_fileno = None
+    __wss6_fileno = None
+
+    def create_listener(self):
+        local_configs = self.configs.get("local", {})
+
+        enable_ipv6 = bool(int(local_configs.get("enable_ipv6", 0)))
+        listen_ip = local_configs.get("listen_ip", "0.0.0.0")
+        listen_ipv6 = local_configs.get("listen_ipv6", "::")
+        listen_port = int(local_configs.get("listen_port", 8000))
+
+        if enable_ipv6:
+            self.__wss6_fileno = self.create_handler(-1, wsserver.ws_listener, (listen_ipv6, listen_port,),
+                                                     is_ipv6=True)
+            self.get_handler(self.__wss6_fileno).after()
+
+        self.__wss_fileno = self.create_handler(-1, wsserver.ws_listener, (listen_ip, listen_port,), is_ipv6=False)
+        self.get_handler(self.__wss_fileno).after()
+
     def any2ws_init(self):
         self.set_sysroot(BASE_DIR)
+        self.load_configs("server.ini")
+        self.__wss_fileno = -1
+        self.__wss6_fileno = -1
 
     def any2ws_release(self):
         pass
-
-    @property
-    def conn_timeout(self):
-        return 300
 
 
 def main():
