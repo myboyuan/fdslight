b73ca21ae57397d6ff174926a4f89165def3065d
fdslight <freekai@outlook.com>
2019-06-10 00:42:06 +0800

    ...

--- freenet/handlers/tunnelc.py
+++ freenet/handlers/tunnelc.py
@@ -47,7 +47,8 @@ class tcp_tunnel(tcp_handler.tcp_handler):
         self.__over_https = kwargs.get("tunnel_over_https", False)
 
         if self.__over_https:
-            context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+            context = ssl.create_default_context()
+            context.options &= ~ssl.OP_NO_SSLv3
             context.set_alpn_protocols(["http/1.1"])
             s = context.wrap_socket(s, do_handshake_on_connect=False)
 
