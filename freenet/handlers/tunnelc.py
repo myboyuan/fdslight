edcd92ced63c172075c0ef7fc644ec6fe770a7f2
fdslight <freekai@outlook.com>
2019-06-09 14:03:05 +0800

    ...

--- freenet/handlers/tunnelc.py
+++ freenet/handlers/tunnelc.py
@@ -265,15 +265,17 @@ class tcp_tunnel(tcp_handler.tcp_handler):
 
         # 伪装成websocket握手
         kv_pairs = [("Connection", "Upgrade"), ("Upgrade", "websocket",), (
-        "User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:67.0) Gecko/20100101 Firefox/67.0",),
+            "User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:67.0) Gecko/20100101 Firefox/67.0",),
                     ("Accept-Language", "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2"),
                     ("X-Auth-Id", auth_id,), ("Sec-WebSocket-Version", 13,), ("Sec-WebSocket-Key", self.rand_string(),),
                     ("Sec-WebSocket-Protocol", "fdslight")]
 
         if int(self.__server_address[1]) == 443:
             host = ("Host", self.__server_address[0],)
+            origin = ("Origin", "https://%s" % self.__server_address[0])
         else:
             host = ("Host", "%s:%s" % self.__server_address,)
+            origin = ("Origin", "https://%s:%s" % self.__server_address,)
 
         kv_pairs.append(host)
 
