9483eab9c78359d645f71040be322fba4d75719b
fdslight <freekai@outlook.com>
2020-03-28 19:35:53 +0800

    ...

--- freenet/handlers/tunnelc.py
+++ freenet/handlers/tunnelc.py
@@ -71,6 +71,7 @@ class tcp_tunnel(tcp_handler.tcp_handler):
             if self.__strict_https:
                 context.verify_mode = ssl.CERT_REQUIRED
                 context.load_verify_locations(self.dispatcher.ca_path)
+                print("enable strict https")
             else:
                 context.verify_mode = ssl.CERT_NONE
 
