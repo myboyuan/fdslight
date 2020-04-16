333f5e12b508185294310228ebb25eed54fa1568
fdslight <freekai@outlook.com>
2020-04-16 23:54:30 +0800

    ...

--- freenet/handlers/LANd_forward.py
+++ freenet/handlers/LANd_forward.py
@@ -55,7 +55,6 @@ class client(ssl_handler.ssl_handler):
 
         logging.print_general("connecting,%s" % auth_id, self.__address)
         self.set_socket(s)
-
         try:
             self.connect(address)
         except:
