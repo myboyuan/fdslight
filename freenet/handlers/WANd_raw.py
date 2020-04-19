4952ce1bcaa2f4b07d0ae15aa5e9e5dfb108de04
fdslight <freekai@outlook.com>
2020-04-19 20:46:55 +0800

    ...

--- freenet/handlers/WANd_raw.py
+++ freenet/handlers/WANd_raw.py
@@ -84,7 +84,7 @@ class handler(tcp_handler.tcp_handler):
         self.add_evt_read(self.fileno)
         self.set_timeout(self.fileno, 10)
 
-        self.tcp_loop_read_num = 1
+        self.tcp_loop_read_num = 100
         logging.print_general("accepted", self.__caddr)
 
         return self.fileno
