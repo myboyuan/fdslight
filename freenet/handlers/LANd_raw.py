7ba07c9b679939c287b49b2602fa8234e2d66931
fdslight <freekai@outlook.com>
2020-04-20 23:53:42 +0800

    ...

--- freenet/handlers/LANd_raw.py
+++ freenet/handlers/LANd_raw.py
@@ -58,13 +58,11 @@ class client(tcp_handler.tcp_handler):
         t = time.time()
         # 限制300s没数据那么就关闭连接
         if t - self.__time > 300:
-            print("BBBB")
             self.get_handler(self.__creator).tell_forwarding_close()
             return
         self.set_timeout(self.fileno, 10)
 
     def tcp_error(self):
-        print("AAAA")
         self.get_handler(self.__creator).tell_forwarding_close()
 
     def tcp_delete(self):
