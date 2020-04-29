2553239564acaf3d93338b2ce89dbc41a6563e1b
fdslight <freekai@outlook.com>
2020-04-29 18:53:51 +0800

    ...

--- LANd_pass.py
+++ LANd_pass.py
@@ -196,11 +196,11 @@ def start(debug, wol_key, wol_port, wol_bind_ip):
     try:
         cls.ioloop(wol_key, wol_bind_ip=wol_bind_ip, debug=debug, wol_port=wol_port)
     except KeyboardInterrupt:
-        if os.path.exists(PID_PATH): os.remove(PID_PATH)
+        if os.path.isfile(PID_PATH): os.remove(PID_PATH)
         cls.release()
         sys.exit(0)
     except:
-        if os.path.exists(PID_PATH): os.remove(PID_PATH)
+        if os.path.isfile(PID_PATH): os.remove(PID_PATH)
         cls.release()
         logging.print_error()
         sys.exit(-1)
