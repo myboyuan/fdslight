b7fc5a8e18c6a5539535dbd63cd20dc938fe824b
fdslight <freekai@outlook.com>
2020-05-29 21:41:00 +0800

    ...

--- freenet/lib/gw.c
+++ freenet/lib/gw.c
@@ -39,10 +39,13 @@ gw_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 {
     u_int32_t pre_alloc_mbuf,pre_alloc_qos_slot;
     fdsl_gw *self;
+    struct nm_desc *netmap;
 
     const char *tap_name;
     const char *if_name;
     char tap_name2[512];
+
+    tap_name2[0]='\0';
 
     self=(fdsl_gw *)type->tp_alloc(type,0);
     if(NULL==self) return NULL;
@@ -54,4 +57,46 @@ gw_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 
     return NULL;
 }
+
+
+static PyMethodDef gw_methods[]={
+    {NULL}
+};
+
+static PyTypeObject gw_type={
+    PyVarObject_HEAD_INIT(NULL,0)
+    .tp_new=gw_new,
+    .tp_dealloc=(destructor)gw_dealloc,
+    .tp_name="gw",
+    .tp_doc="gw",
+    .tp_basicsize=sizeof(fdsl_gw),
+    .tp_itemsize=0,
+    .tp_flags=Py_TPFLAGS_DEFAULT,
+    .tp_methods=gw_methods
+};
+
+static PyModuleDef gw_module={
+    PyModuleDef_HEAD_INIT,
+    "gw",
+    "gateway module",
+    -1,
+    NULL
+};
+
+PyMODINIT_FUNC
+PyInit_gw(void){
+    PyObject* m;
+
+    if (PyType_Ready(&gw_type) < 0) return NULL;
+
+    m = PyModule_Create(&gw_module);
+    if (m == NULL) return NULL;
+
+    Py_INCREF(&gw_type);
+    PyModule_AddObject(m, "gw", (PyObject *)&gw_type);
+
+    return m;
+}
+
+
 
