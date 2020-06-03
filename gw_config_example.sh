ef102552aabb623a4439176eeccf6726a6527ae1
fdslight <freekai@outlook.com>
2020-06-04 00:03:05 +0800

    home gateway

--- gw_config_example.sh
+++ gw_config_example.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-insmod ./driver/xt_FULLCONENAT.ko
 
 iptables -t nat -A POSTROUTING -s 192.168.1.0/24 -o eth0 -j MASQUERADE
 iptables -A FORWARD -s 192.168.1.0/24 -j ACCEPT
+
+insmod ./driver/xt_FULLCONENAT.ko
+
